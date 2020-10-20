fe5faf5e9da71e7b54d5eb0ca64df706e27d7b4c
Yeoma <Yeoman_M@126.com>
2020-10-20 15:46:58 +0800

    gooooooooo

--- ch03/classfile/cp_numeric.go
+++ ch03/classfile/cp_numeric.go
@@ -1,5 +1,7 @@
 package classfile
 
+import "math"
+
 type ConstantIntegerInfo struct {
 	val int32
 }
@@ -12,8 +14,22 @@ type ConstantFloatInfo struct {
 	val float32
 }
 
-func (self *ConstantFloatInfo) readInfo(reader *ClassReader)  {
-	self.val =
+func (self *ConstantFloatInfo) readInfo(reader *ClassReader) {
+	self.val = math.Float32frombits(reader.readUint32())
 }
 
+type ConstantLongInfo struct {
+	val int64
+}
 
+func (self *ConstantLongInfo) readInfo(reader *ClassReader) {
+	self.val = int64(reader.readUint64())
+}
+
+type COnstantDoubleInfo struct {
+	val float64
+}
+
+func (self *COnstantDoubleInfo) readInfo(reader *ClassReader) {
+	self.val = math.Float64frombits(reader.readUint64())
+}
